d908690b73e35bb2aebb3bbdc6555b9163c9a0f4
Aresla <areslas@list.ru>
2023-09-03 12:24:12 +0300

    add day and time selector

--- scripts/pesukone.js
+++ scripts/pesukone.js
@@ -1,17 +1,18 @@
+const FIRST_HOUR = 8;
+const LAST_HOUR = 21;
+let isCurrentMonthSelected;
 const getMonths = () => {
     // getting next 2 month dynamically
     // generate month strings Month - Year:  August 2023
     // append options to the select on the page
-
     const monthSelectElement = document.getElementById("kuukaudet");
     const textMonth = document.createElement('option');
     textMonth.innerText = "--kuukaudet--";
     monthSelectElement.appendChild(textMonth);
 
     const today = new Date();
-    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    const months = ["Tammikuu","Helmikuu","Maaliskuu","Huhtikuu","Toukokuu","Kesäkuu","Heinäkuu","Elokuu","Syyskuu","Lokakuu","Marraskuu","Joulukuu"];
 
-    // TODO: next year to handle
     let monthNow = months[today.getMonth()];
     let nextMonth = months[today.getMonth() + 1];
     let yearNow = today.getFullYear();
@@ -20,7 +21,6 @@ const getMonths = () => {
 
     const optionOne = document.createElement('option');
     optionOne.innerText = `${dateToStrOne}`;
-    optionOne.value = monthNow;
     optionOne.value = `${yearNow}-${today.getMonth()}`;
     monthSelectElement.appendChild(optionOne);
 
@@ -34,49 +34,76 @@ const getMonths = () => {
     textDay.innerText = "--päivät--";
     parentDayElement.appendChild(textDay);
 
+    const parentTimeElement = document.getElementById("ajat");
+    const textTime = document.createElement('option');
+    textTime.innerText = "--ajat--";
+    parentTimeElement.appendChild(textTime);
+
     monthSelectElement.addEventListener('change', handleSelectMonth, false);
-
-
 }
-
-
 
 const handleSelectMonth = (event) => {
     const selectedOptionValue = event.target.value;
     const [year, month] = selectedOptionValue.split('-');
-    const yearParsed = parseInt(year, 10);
-    const monthParsed = parseInt(month, 10);
-    console.log('month selected type of: ', typeof month);
-    const dayInMonthCount = new Date(yearParsed, monthParsed + 1, 0).getDate();
-    console.log('dayInMonthCount: ', dayInMonthCount);
-    const isCurrentMonthSelected = true;
+    const monthParsed = parseInt(month, 10);   // string to integer
 
+    let date  = new Date();
+    var monthToday = date.getMonth();
+    if (monthParsed == monthToday) {
+        isCurrentMonthSelected = true;
+    } else {
+        isCurrentMonthSelected = false;
+    }
     const daySelectElement = document.getElementById("päivät");
-    const textDay = document.createElement('option');
-    textDay.innerText = "--päivät--";
-    daySelectElement.appendChild(textDay);
-
-    daySelectElement.setAttribute('disabled', false);
-
-
+    daySelectElement.removeAttribute('disabled');
+    getDays();
 }
 
 const getDays = () => {
-    let day = today.getDate();
-    const days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+    const today = new Date();
+    const dayToday = today.getDate();
+    const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+    const lastDay = lastDayOfMonth.getDate();
+    const daySelectElement = document.getElementById("päivät");
+    if (isCurrentMonthSelected) {
+        for (let i = dayToday; i <= lastDay; i++) {
+            let day = document.createElement('option');
+            day.innerText = `${i}`;
+            daySelectElement.appendChild(day); 
+        }
+    }
+    else {
+        for (let i = 1; i <= lastDay; i++) {
+            let day = document.createElement('option');
+            day.innerText = `${i}`;
+            daySelectElement.appendChild(day); 
+        }
+    }
+    daySelectElement.addEventListener('change', handleSelectDays, false);        
 }
 
+const handleSelectDays = (event) => {
+    const selectedOptionValue = event.target.value;
+
+    const timeSelectElement = document.getElementById("ajat");
+    timeSelectElement.removeAttribute('disabled');
+    getTime(); 
+}
+
+const getTime = () => {
+    const timeSelectElement = document.getElementById("ajat");
+    for (let i = FIRST_HOUR; i <= LAST_HOUR; i++) {
+        let option= document.createElement('option');
+        let periodOfTime = document.createElement('span');
+        periodOfTime.textContent = `${i}:00 - ${i + 1}:00`;
+        option.appendChild(periodOfTime);
+        if (i == 19 || i == 18) {
+            option.setAttribute('disabled', true);
+        }
+        timeSelectElement.appendChild(option);
+    }
+}
 
 const initPesukone = () => {
     getMonths();
 }
-
-
-// develop the functions to be used as month select listener
-// it will generate days and append to the day listener
-
-
-// develop a function to be used as day select listener
-// it will get the booking for the day
-// and show the table with day slots and if some of them available
-
